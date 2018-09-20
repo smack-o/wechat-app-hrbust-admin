32b1e5a61e0309050fd8838977f2f0a8ef0db857
smackgg <smackgg@gmail.com>
2018-09-20 12:49:30 +0800

    add element ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
